2d6c3db00b24e9b6637ddc255c759e523cef69c4
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2024-04-03 20:34:24 +0530

    UI Completed

--- app/src/main/java/com/example/pictoria/MainActivity.kt
+++ app/src/main/java/com/example/pictoria/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.pictoria
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var currentImage = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar?.hide()
+        val prev = findViewById<ImageButton>(R.id.btnPrev)
+        val next = findViewById<ImageButton>(R.id.btnNext)
+
+
     }
 }
