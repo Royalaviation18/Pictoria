e81f2f01ba27929925609ff2aada2ec37a431f77
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2024-04-04 07:18:54 +0530

    Feat : Previous Button is up and running

--- app/src/main/java/com/example/pictoria/MainActivity.kt
+++ app/src/main/java/com/example/pictoria/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.pictoria
 
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     var currentImage = 0
+    lateinit var image: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +25,25 @@ class MainActivity : AppCompatActivity() {
         val prev = findViewById<ImageButton>(R.id.btnPrev)
         val next = findViewById<ImageButton>(R.id.btnNext)
 
+        prev.setOnClickListener {
+            //current Image Id in string
+            val idCurrentImage = "ivMotivation$currentImage"
+            //converting the String to int address associated with it
+            //idCurrentImage is the string type, id is the thing which we are trying to convert (or with we are currently working)
+            val idCurrentImageIntFormat =
+                this.resources.getIdentifier(idCurrentImage, "id", packageName)
+            image = findViewById(idCurrentImageIntFormat)
+            image.alpha = 0f
 
+            //handling the corner case from 0-> to 4th image
+            currentImage = (5 + currentImage - 1) % 5
+            val idImageToShowString = "ivMotivation$currentImage"
+            val idImagetoShowInt =
+                this.resources.getIdentifier(idImageToShowString, "id", packageName)
+            image = findViewById(idImagetoShowInt)
+            image.alpha = 1f
+        }
     }
+
+
 }
